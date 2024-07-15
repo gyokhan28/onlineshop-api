f544d09868e1a1bf82f287a6a489294e487dcd14
Gyokhan <gyokhan28@gmail.com>
2024-07-15 12:07:46 +0300

    Add mapper for Employee class

---
+++ src/main/java/com/example/online_shop_api/Mapper/EmployeeMapper.java
@@ -0,0 +1,64 @@
+package com.example.online_shop_api.Mapper;
+
+import com.example.online_shop_api.Dto.Request.EmployeeRequestDto;
+import com.example.online_shop_api.Dto.Response.EmployeeResponseDto;
+import com.example.online_shop_api.Entity.Employee;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class EmployeeMapper {
+
+    public EmployeeResponseDto toDto(Employee employee) {
+        if (employee == null) {
+            return null;
+        }
+
+        EmployeeResponseDto employeeResponseDto = new EmployeeResponseDto();
+
+        employeeResponseDto.setDateOfBirth(employee.getDateOfBirth());
+        employeeResponseDto.setId(employee.getId());
+        employeeResponseDto.setFirstName(employee.getFirstName());
+        employeeResponseDto.setLastName(employee.getLastName());
+        employeeResponseDto.setUsername(employee.getUsername());
+        employeeResponseDto.setEmail(employee.getEmail());
+        employeeResponseDto.setSalary(employee.getSalary());
+        employeeResponseDto.setPhoneNumber(employee.getPhoneNumber());
+        employeeResponseDto.setRole(employee.getRole());
+        employeeResponseDto.setCreatedAt(employee.getCreatedAt());
+        employeeResponseDto.setEnabled(employee.isEnabled());
+        employeeResponseDto.setAge(calculateAge(employee.getDateOfBirth()));
+
+        return employeeResponseDto;
+    }
+
+    public Employee toEntity(EmployeeRequestDto employeeRequestDto) {
+        if(employeeRequestDto == null){
+            return null;
+        }
+
+        Employee.EmployeeBuilder employee = Employee.builder();
+
+        employee.firstName(employeeRequestDto.getFirstName());
+        employee.lastName(employeeRequestDto.getLastName());
+        employee.username(employeeRequestDto.getUsername());
+        employee.email(employeeRequestDto.getEmail());
+        employee.password(employeeRequestDto.getPassword());
+        employee.dateOfBirth(employeeRequestDto.getDateOfBirth());
+        employee.salary(employeeRequestDto.getSalary());
+        employee.phoneNumber(employeeRequestDto.getPhoneNumber());
+
+        employee.createdAt(java.time.LocalDateTime.now());
+        employee.isEnabled(true);
+
+        return employee.build();
+    }
+
+    public int calculateAge(LocalDate birthDate) {
+        if (birthDate != null) {
+            LocalDate currentDate = LocalDate.now();
+            return Period.between(birthDate, currentDate).getYears();
+        }
+        return 0;
+    }
+}
--- src/main/java/com/example/online_shop_api/Service/EmployeeService.java
+++ src/main/java/com/example/online_shop_api/Service/EmployeeService.java
@@ -2,15 +2,20 @@ package com.example.online_shop_api.Service;
 
 import com.example.online_shop_api.Dto.Request.EmployeeRequestDto;
 import com.example.online_shop_api.Entity.Employee;
+import com.example.online_shop_api.Entity.Role;
+import com.example.online_shop_api.Exceptions.*;
 import com.example.online_shop_api.Repository.EmployeeRepository;
+import com.example.online_shop_api.Static.RoleType;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
