a5e19cfc0b7f5c156624619701fae67f8295a9b1
Gyokhan <gyokhan28@gmail.com>
2024-07-20 00:40:52 +0300

    Bug fix for single product show endpoint

--- src/main/java/com/example/online_shop_api/Repository/OrderProductRepository.java
+++ src/main/java/com/example/online_shop_api/Repository/OrderProductRepository.java
@@ -3,6 +3,7 @@ package com.example.online_shop_api.Repository;
 import com.example.online_shop_api.Entity.OrderProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,7 +11,7 @@ public interface OrderProductRepository extends JpaRepository<OrderProduct, Long
 
     // Will be used to calculate total price and display the basket order for the user.
     @Query("SELECT new com.example.online_shop_api.Entity.OrderProduct(op.product.id, op.product.name, op.product.price, op.quantity) FROM OrderProduct op WHERE op.order.id = :orderId")
-    List<OrderProduct> findAllByOrderId(Long orderId);
+    List<OrderProduct> findAllByOrderId(@Param("orderId")Long orderId);
     //[OrderProduct(id=null, order=null, product=Product(id=1, name=name1, price=2.10, quantity=0, imageLocation=null), quantity=3),
     // OrderProduct(id=null, order=null, product=Product(id=2, name=name2, price=2.20, quantity=0, imageLocation=null), quantity=1)]
 
