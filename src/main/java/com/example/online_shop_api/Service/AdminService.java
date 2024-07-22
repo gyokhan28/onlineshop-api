e83c3e0b80153e927b04fba072387e8e68d12b91
Yordanofff <57867535+Yordanofff@users.noreply.github.com>
2024-07-22 09:06:48 +0300

    Update AdminService.java
    Add extra checks + allow each one of the fields to be set separately.

--- src/main/java/com/example/online_shop_api/Service/AdminService.java
+++ src/main/java/com/example/online_shop_api/Service/AdminService.java
@@ -1,6 +1,5 @@
 package com.example.online_shop_api.Service;
 
-import com.example.online_shop_api.Dto.Response.ErrorResponse;
 import com.example.online_shop_api.Entity.Employee;
 import com.example.online_shop_api.Repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,18 +21,33 @@ public class AdminService {
         return ResponseEntity.ok(employeeRepository.findByRole_IdNot(1L));
     }
 
-    public ResponseEntity<Boolean> updateEmployeeStatusAndSalary(Long employeeId, boolean isEnabled, String salary){
-        Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
-        if(employeeOptional.isPresent()) {
-            Employee employee = employeeOptional.get();
+    public ResponseEntity<?> updateEmployeeStatusAndSalary(Long employeeId, Boolean isEnabled, String salary) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employeeId);
+
+        // Get rid of this option to make the method simpler to read
+        if (optionalEmployee.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not found");
+        }
+
+        Employee employee = optionalEmployee.get();
+
+        // Check if there's an actual value passed (in case it's only a salary update)
+        if (isEnabled != null) {
             employee.setEnabled(isEnabled);
-            if(salary != null && !salary.isEmpty()){
+        }
+
+        if (salary != null && !salary.isEmpty()) {
+            // Check if the String passed in as a salary is actually a BigDecimal
+            try {
                 BigDecimal salaryValue = new BigDecimal(salary);
                 employee.setSalary(salaryValue);
+            } catch (NumberFormatException e) {
+                return ResponseEntity.badRequest().body("Invalid salary format");
             }
-            employeeRepository.save(employee);
-            return ResponseEntity.ok(true);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
+
+        employeeRepository.save(employee);
+
+        return ResponseEntity.ok("Employee updated successfully");
     }
 }
