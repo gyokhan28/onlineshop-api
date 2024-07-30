04c33e18112e678e374900998dd73d908f2886d4
berki0 <107307903+berki0@users.noreply.github.com>
2024-07-31 00:19:12 +0300

    add tests

--- src/main/java/com/example/online_shop_api/Controller/ProductHelpersController.java
+++ src/main/java/com/example/online_shop_api/Controller/ProductHelpersController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/brand")
-public class BrandController {
+public class ProductHelpersController {
   private final ProductHelpersService productHelpersService;
 
   @GetMapping("/getAllBrand")
