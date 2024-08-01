719a9908b66b48a337f2cf362514119f8cea96f2
berki0 <107307903+berki0@users.noreply.github.com>
2024-08-01 19:37:44 +0300

    add type of Product

--- src/main/java/com/example/online_shop_api/Dto/Response/ProductResponseDto.java
+++ src/main/java/com/example/online_shop_api/Dto/Response/ProductResponseDto.java
@@ -3,14 +3,17 @@ package com.example.online_shop_api.Dto.Response;
 import com.example.online_shop_api.Entity.ProductHelpers.Brand;
 import com.example.online_shop_api.Entity.ProductHelpers.Color;
 import java.math.BigDecimal;
-import lombok.Builder;
-import lombok.Data;
 
-@Data
+import lombok.*;
+
 @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponseDto {
     private Long id;
     private String name;
+    private String productType;
     private BigDecimal price;
     private int quantity;
     private String imageLocation;
--- src/main/java/com/example/online_shop_api/Entity/Products/Product.java
+++ src/main/java/com/example/online_shop_api/Entity/Products/Product.java
@@ -16,39 +16,44 @@ import java.util.List;
 @SuperBuilder
 @NoArgsConstructor
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    protected Long id;
-    protected String name;
-    protected BigDecimal price;
-    protected int quantity;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  protected Long id;
 
-    @Convert(converter = StringListConverter.class)
-    @Column(name = "image_urls", columnDefinition = "TEXT")
-    protected List<String> imageUrls;
+  @Column(name = "product", insertable = false, updatable = false)
+  protected String productType;
 
-    protected boolean isDeleted;
+  protected String name;
+  protected BigDecimal price;
+  protected int quantity;
 
-    public Product(String name, BigDecimal price, int quantity, List<String> imageUrls) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.imageUrls = imageUrls;
-        this.isDeleted = false;
+  @Convert(converter = StringListConverter.class)
+  @Column(name = "image_urls", columnDefinition = "TEXT")
+  protected List<String> imageUrls;
+
+  protected boolean isDeleted;
+
+  public Product(String name, BigDecimal price, int quantity, List<String> imageUrls) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+    this.imageUrls = imageUrls;
+    this.isDeleted = false;
+  }
+
+  public void setImageLocations(List<String> imageUrls) {
+    // Set the image to null if not set in the frontend so that it doesn't show up an empty/missing
+    // picture
+    if (imageUrls.isEmpty()) {
+      imageUrls = null;
     }
+    this.imageUrls = imageUrls;
+  }
 
-    public void setImageLocations(List<String> imageUrls) {
-        // Set the image to null if not set in the frontend so that it doesn't show up an empty/missing picture
-        if (imageUrls.isEmpty()) {
-            imageUrls = null;
-        }
-        this.imageUrls = imageUrls;
+  public void addImageUrl(String imageUrl) {
+    if (!imageUrl.isBlank()) {
+      this.imageUrls.add(imageUrl);
     }
-
-    public void addImageUrl(String imageUrl) {
-        if (!imageUrl.isBlank()) {
-            this.imageUrls.add(imageUrl);
-        }
-    }
+  }
 }
