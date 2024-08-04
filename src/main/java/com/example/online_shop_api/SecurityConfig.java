29454f8d855fbee12f40c29a8060454f82db97ee
Gyokhan <gyokhan28@gmail.com>
2024-08-04 13:12:32 +0300

    migrate to gradle + remove sonar tests for entities, dtos, mappers, static classes.

--- src/main/java/com/example/online_shop_api/SecurityConfig.java
+++ src/main/java/com/example/online_shop_api/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.example.online_shop_api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
