55aae085c2bcb3d7a34f21481e3c59ceb3fed590
aganchev <alexander.m.ganchev@gmail.com>
2024-08-06 02:44:03 +0300

    changes in build.yml

--- src/test/java/com/example/OnlineShopApiApplication/AdminControllerTest.java
+++ src/test/java/com/example/OnlineShopApiApplication/AdminControllerTest.java
@@ -9,14 +9,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.math.BigDecimal;
 import java.util.Optional;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 
 @SpringBootTest(classes = OnlineShopApiApplication.class)
 @AutoConfigureMockMvc(addFilters = false)
